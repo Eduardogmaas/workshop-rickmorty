64efa50d7cc56f3b8a7e84a6e01ed8e380b62330
Eduardogmaas <eduardogmaas@gmail.com>
2026-05-27 22:35:27 -0300

    h

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -2,11 +2,24 @@ import { useEffect } from "react";
 import CharacterServices from "./CharacterServices";
 
 export default function Characters(){
+    const [data, setData] = useState()
+
     useEffect(() => {
-        CharacterServices.list().then((responce) => {
-            console.log(responce)
+        CharacterServices.list().then((response) => {
+            // console.log(response)
+            setData(response.data)
         })
     },[])
+    
+    function renderData(){
+        if(data != 0){
+            return data?.map((item) =>)
+                return(
+                    <p>{item.name}</p>
+                    <img src={item.image} alt ={}
+                )
+        }
+    }
     
     return <>essa é a tela Characters</>;
 }
